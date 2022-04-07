ce2aadb3fa73a9a02a154997ee76e73d39f69edc
Mithaecus <hey@eddy.ninja>
2022-04-07 11:10:53 -0700

    Ctrate all entity-category relations

--- src/relations.rkt
+++ src/relations.rkt
@@ -1,22 +1,26 @@
 #lang racket
 
-(require racklog)
-
-(require json)
+(require json racklog)
 
 (define %category %empty-rel)
 
 (define (read-json-wrapper filename)
-  (call-with-input-file filename read-json))
+    (call-with-input-file filename read-json))
 
 (define (create-entity-category-relations list-of-files)
-    (begin
-        (define file-name (first list-of-files))
-        (define data (read-json-wrapper file-name))
-        (%assert! %category ()
-            [((string->symbol (hash-ref data 'entity_alias)) 
-              (string->symbol (hash-ref data 'category)))])
-        (%which ()
-            (%category 'bread 'bakery))))
+    (if (null? list-of-files)
+        (void)
+        (begin
+          (let ()
+            (define file-name (first list-of-files))
+            (define data (read-json-wrapper file-name))
+            (%assert! %category ()
+                [((string->symbol (hash-ref data 'entity_alias)) 
+                  (string->symbol (hash-ref data 'category)))])
+            (create-entity-category-relations (list-tail list-of-files 0))))))
 
-(create-entity-category-relations '("./data/flavordb/2.json"))
+(create-entity-category-relations 
+    (map path->string (directory-list "./data/flavordb/" #:build? #t)))
+
+(%which ()
+    (%category 'bread 'bakery))
