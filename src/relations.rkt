02007dfc33d3a0798c4f3969b6d2b477452cddef
Mithaecus <hey@eddy.ninja>
2022-04-07 21:43:02 -0700

    Relations work

--- src/relations.rkt
+++ src/relations.rkt
@@ -2,25 +2,35 @@
 
 (require json racklog)
 
+; Global State
+(define *DATA* (make-hash))
 (define %category %empty-rel)
 
-(define (read-json-wrapper filename)
-    (call-with-input-file filename read-json))
-
-(define (create-entity-category-relations list-of-files)
+; Load json into *DATA*
+(define (load-data list-of-files)
     (if (null? list-of-files)
         (void)
         (begin
-          (let ()
-            (define file-name (first list-of-files))
-            (define data (read-json-wrapper file-name))
-            (%assert! %category ()
-                [((string->symbol (hash-ref data 'entity_alias)) 
-                  (string->symbol (hash-ref data 'category)))])
-            (create-entity-category-relations (list-tail list-of-files 0))))))
+            (let ()
+                (define file-name (car list-of-files))
+                (define file-data (call-with-input-file file-name read-json))
+                (hash-set! *DATA* (string->symbol (hash-ref file-data 'entity_alias)) file-data)
+                (load-data (cdr list-of-files))))))
 
-(create-entity-category-relations 
-    (map path->string (directory-list "./data/flavordb/" #:build? #t)))
+; Build relations between entity_alias and category in %category
+(define (create-entity-category-relations data)
+    (if (null? data)
+        (void)
+        (begin
+          (let ()
+            (define a (string->symbol (hash-ref (hash-ref *DATA* (car data)) 'entity_alias)))
+            (define b (string->symbol (hash-ref (hash-ref *DATA* (car data)) 'category)))
+            (%assert! %category () ([a b]))
+            (create-entity-category-relations (cdr data))))))
+
+; Do the work
+(load-data (map path->string (directory-list "./data/flavordb/" #:build? #t)))
+(create-entity-category-relations (hash-keys *DATA*))
 
 (%which ()
     (%category 'bread 'bakery))
